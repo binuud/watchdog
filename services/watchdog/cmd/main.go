5c135682c06b1184a8e3856b349e4f61ab155d36
Binu Udayakumar <binu@dronasys.com>
2025-03-19 12:20:49 +0530

    WP-1 project init, go mod init, directories, placeholder make and dockerfile

---
+++ services/watchdog/cmd/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "github.com/sirupsen/logrus"
+
+func main() {
+
+	// created using https://www.fancytextpro.com/BigTextGenerator/Cyberlarge
+	logrus.Printf(`
+ _  _  _ _______ _______ _______ _     _ ______   _____   ______
+ |  |  | |_____|    |    |       |_____| |     \ |     | |  ____
+ |__|__| |     |    |    |_____  |     | |_____/ |_____| |_____|
+                                                                
+    `)
+
+}
